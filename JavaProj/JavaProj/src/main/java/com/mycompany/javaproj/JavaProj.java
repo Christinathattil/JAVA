7158882d6e2cf480ffe5107ac5c1b2e08ea44395
Christina J <73300138+Christinathattil@users.noreply.github.com>
2024-01-28 00:27:37 +0530

    Add files via upload

---
+++ JavaProj/JavaProj/src/main/java/com/mycompany/javaproj/JavaProj.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.javaproj;
+/**
+ *
+ * @author kristyjs
+ */
+public class JavaProj {
+    
+    public static void main(String[] args) {
+              
+        Login LoginFrame = new Login();
+        LoginFrame.setVisible(true);
+        LoginFrame.pack();
+        LoginFrame.setLocationRelativeTo(null);
+    }
+    
+}
